e29340c9ed642c58e95133afbe490db720ee42e0
OdidaProtas <bryodiiidah@gmail.com>
2021-10-31 15:09:41 +0300

    update product status

--- src/entity/Product.ts
+++ src/entity/Product.ts
@@ -1,6 +1,12 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import Store from "./Store";
 
+export enum ProductStatus {
+  "draft" = "draft",
+  "active" = "active",
+  "archived" = "archived",
+}
+
 @Entity()
 export class Product {
   @PrimaryGeneratedColumn("uuid")
@@ -10,9 +16,16 @@ export class Product {
   title: string;
 
   @Column({
-    nullable:true
+    nullable: true,
   })
   desc: string;
+
+  @Column({
+    type: "enum",
+    enum: ProductStatus,
+    default: ProductStatus.draft,
+  })
+  status: ProductStatus;
 
   @ManyToOne(() => Store, (store) => store.products)
   store: Store;
